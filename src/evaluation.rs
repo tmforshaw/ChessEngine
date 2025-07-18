38bed28c2362f186a9e59ca3f8ce8c0f695d783c
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2025-07-18 14:32:11 +0100

    Begin to parse UCI commands, and perform bestmove generation

---
+++ src/evaluation.rs
@@ -0,0 +1,27 @@
+use chess_core::{
+    board::{Board, Player},
+    piece::Piece,
+};
+
+pub fn piece_to_value(piece: Piece) -> i32 {
+    match piece {
+        Piece::WPawn | Piece::BPawn => 100,
+        Piece::WKnight | Piece::BKnight => 320,
+        Piece::WBishop | Piece::BBishop => 330,
+        Piece::WRook | Piece::BRook => 500,
+        Piece::WQueen | Piece::BQueen => 500,
+        _ => 0,
+    }
+}
+
+pub fn material(board: &Board, player: Player) -> i32 {
+    board
+        .get_all_player_pieces(player)
+        .into_iter()
+        .map(|(piece, _)| piece)
+        .fold(0, |acc, piece| acc + piece_to_value(piece))
+}
+
+pub fn evaluate(board: &Board) -> i32 {
+    material(board, Player::White) - material(board, Player::Black)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,183 @@
+use std::io::{self, BufRead, Write};
+
+use chess_core::{board::Board, piece_move::PieceMove};
+
+use crate::evaluation::evaluate;
+
+pub mod evaluation;
+
+const ENGINE_NAME: &str = "tmforshaw_engine";
+const AUTHOR_NAME: &str = "tmforshaw";
+
 fn main() {
-    println!("Hello, world!");
+    respond_to_uci();
+}
+
+#[test]
+fn board_move_test() {
+    let mut board = Board::default();
+
+    board.apply_move(PieceMove::from_algebraic("e2e4").unwrap());
+    println!("{}\n", board.positions);
+    board.undo_move();
+    println!("{}\n", board.positions);
+
+    assert!(board.positions == Board::default().positions);
+}
+
+pub fn respond_to_uci() {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    let mut board = None;
+
+    let lines = "uci\nposition startpos moves e2e4 e7e5\ngo".lines();
+
+    for line in lines {
+        // stdin.lock().lines() {
+        // let line = line.unwrap();
+        let tokens: Vec<_> = line.split_whitespace().collect();
+        if tokens.is_empty() {
+            continue;
+        }
+
+        match tokens[0] {
+            "uci" => {
+                writeln!(stdout, "id name {ENGINE_NAME}").unwrap();
+                writeln!(stdout, "id author {AUTHOR_NAME}").unwrap();
+                writeln!(stdout, "uciok").unwrap();
+            }
+            "isready" => {
+                writeln!(stdout, "readyok").unwrap();
+            }
+            "position" => {
+                board = handle_position_uci(&tokens[1..]);
+            }
+            "go" => {
+                let mut best_move = None;
+                if let Some(board) = &mut board {
+                    best_move = handle_go_uci(board);
+                }
+
+                match best_move {
+                    Some(best_move) => {
+                        writeln!(stdout, "bestmove {}", best_move.to_algebraic().unwrap()).unwrap()
+                    }
+                    None => todo!(),
+                };
+            }
+            "quit" => {
+                break;
+            }
+            _ => {
+                // Unknown command
+            }
+        }
+
+        stdout.flush().unwrap();
+    }
+}
+
+pub fn handle_position_uci(tokens: &[&str]) -> Option<Board> {
+    match tokens[0] {
+        "fen" => {
+            let fen_string = tokens[1];
+
+            let mut board = Board::from_fen(fen_string).unwrap();
+
+            if let Some(&moves_keyword) = tokens.get(2) {
+                if moves_keyword == "moves" {
+                    handle_moves_uci(&mut board, &tokens[3..]);
+                }
+            }
+
+            Some(board)
+        }
+        "startpos" => {
+            let mut board = Board::default();
+
+            if let Some(&moves_keyword) = tokens.get(1) {
+                if moves_keyword == "moves" {
+                    handle_moves_uci(&mut board, &tokens[2..]);
+                }
+            }
+
+            Some(board)
+        }
+        _ => {
+            // Should never happen
+            unreachable!()
+        }
+    }
+}
+
+pub fn handle_moves_uci(board: &mut Board, moves: &[&str]) {
+    let moves = moves
+        .iter()
+        .map(|&move_str| PieceMove::from_algebraic(move_str))
+        .filter_map(Result::ok)
+        .collect::<Vec<_>>();
+
+    for piece_move in moves {
+        board.apply_move(piece_move);
+        println!("{}\n", board.positions);
+    }
+}
+
+pub fn handle_go_uci(board: &mut Board) -> Option<PieceMove> {
+    let mut best_move = None;
+    let mut best_score = i32::MIN;
+
+    let search_depth = 3;
+
+    for piece_move in board.get_all_possible_moves(board.get_player()) {
+        board.apply_move(piece_move);
+        let score = alpha_beta(board, search_depth - 1, i32::MIN, i32::MAX, false);
+        board.undo_move();
+
+        if score > best_score {
+            best_score = score;
+            best_move = Some(piece_move);
+        }
+    }
+
+    best_move
+}
+
+pub fn alpha_beta(
+    board: &mut Board,
+    depth: i32,
+    mut alpha: i32,
+    mut beta: i32,
+    maximising: bool,
+) -> i32 {
+    if depth <= 0 || board.has_game_ended().is_some() {
+        return evaluate(board);
+    }
+
+    let mut best_score = if maximising { i32::MIN } else { i32::MAX };
+
+    for piece_move in board.get_all_possible_moves(board.get_player()) {
+        board.apply_move(piece_move);
+
+        let score = alpha_beta(board, depth - 1, alpha, beta, !maximising);
+
+        board.undo_move();
+
+        if maximising {
+            best_score = best_score.max(score);
+            alpha = alpha.max(score);
+            if alpha >= beta {
+                break; // Beta cut‑off
+            }
+        } else {
+            best_score = best_score.min(score);
+            beta = beta.min(score);
+            if beta <= alpha {
+                break; // Alpha cut‑off
+            }
+        }
+    }
+
+    best_score
 }
